74befc6333317e344fb6a0e9f1906b4739a0f9fa
Sathish Kariyanna <38923455+sathishkariyanna@users.noreply.github.com>
2019-03-07 12:25:36 -0600

    updating the code to enhance swgger2 api documentation

--- spring-boot-restful-service-swagger2/src/main/java/com/sathish/springboot/restapi/controller/PersonController.java
+++ spring-boot-restful-service-swagger2/src/main/java/com/sathish/springboot/restapi/controller/PersonController.java
@@ -6,15 +6,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sathish.springboot.restapi.dto.Person;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
 /**
  * 
  * @author Sathish
  *
  */
 @RestController
+@Api(value = "some company", description = "person description") // to display on swagger2 UI
 public class PersonController {
-	//swaggger URL http://localhost:8080/swagger-ui.html
+	// swaggger URL http://localhost:8080/swagger-ui.html
 	// URL http://localhost:8080/rest/person
+	@ApiOperation(value = "View list of persons", response = PersonController.class) // to display on swagger2 UI
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved person"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
 	@RequestMapping(value = "/rest/person", method = RequestMethod.GET)
 	public Person Person() {
 		return new Person("Sathish Kariyanna");

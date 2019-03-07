2d36b755c199c564b44e5eb6d4387bf064c2e96b
Sathish Kariyanna <38923455+sathishkariyanna@users.noreply.github.com>
2019-03-07 12:23:08 -0600

    updating the code to enhance swgger2 api documentation

--- spring-boot-restful-service-swagger2/src/main/java/com/sathish/springboot/restapi/config/SwaggerConfig.java
+++ spring-boot-restful-service-swagger2/src/main/java/com/sathish/springboot/restapi/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,11 +18,29 @@ import static springfox.documentation.builders.PathSelectors.regex;
  */
 @Configuration
 @EnableSwagger2
+/*
+ * //this is the minimum configuration to use swagger2 public class
+ * SwaggerConfig {
+ * 
+ * @Bean public Docket productApi() { return new
+ * Docket(DocumentationType.SWAGGER_2).select()
+ * .apis(RequestHandlerSelectors.basePackage(
+ * "com.sathish.springboot.restapi.controller")) .paths(regex("/rest.*"))
+ * .build(); } }
+ */
 public class SwaggerConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.sathish.springboot.restapi.controller"))
-				.paths(regex("/rest.*")).build();
+				.paths(regex("/rest.*")).build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		ApiInfo apiInfo = new ApiInfo("Spring Boot REST API", "Spring Boot REST API for ABC company", "1.0",
+				"Terms of service",
+				new Contact("Sathish Kariyanna", "http://sathishkariyanna.blogspot.com/", "abc@gmail.com"),
+				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+		return apiInfo;
 	}
 }

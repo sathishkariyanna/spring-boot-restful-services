6d4297a0cb907f1c1dc8b40e201181f45f0e381b
Sathish Kariyanna <38923455+sathishkariyanna@users.noreply.github.com>
2019-03-07 12:24:17 -0600

    updating the code to enhance swgger2 api documentation

--- spring-boot-restful-service-swagger2/src/main/java/com/sathish/springboot/restapi/controller/EmployeeController.java
+++ spring-boot-restful-service-swagger2/src/main/java/com/sathish/springboot/restapi/controller/EmployeeController.java
@@ -7,10 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +14,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.sathish.springboot.restapi.dto.Employee;
 import com.sathish.springboot.restapi.service.EmployeeService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * Handles requests for the Employee service.
@@ -31,6 +31,7 @@ import com.sathish.springboot.restapi.service.EmployeeService;
  *
  */
 @RestController
+@Api(value = "some company", description = "Employee description") // to display on swagger2 UI
 public class EmployeeController {
 
 	// Map to store employees, ideally it should come from database
@@ -39,7 +40,9 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
+	// swaggger URL http://localhost:8080/swagger-ui.html
 	// URL http://localhost:8080/rest/emp/default
+	@ApiOperation(value = "View default employee") // to display on swagger2 UI
 	@RequestMapping(value = "/rest/emp/default", method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody Employee getDefaultEmployee() {
 		Employee emp = new Employee();
@@ -49,6 +52,7 @@ public class EmployeeController {
 	}
 
 	// URL http://localhost:8080/rest/emp/9999
+	@ApiOperation(value = "get employee by ID")
 	@RequestMapping(value = "/rest/emp/{id}", method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody Employee getEmployee(@PathVariable("id") int empId) {
 		System.out.println("Start getEmployee. ID=" + empId);
@@ -60,6 +64,7 @@ public class EmployeeController {
 	// URL http://localhost:8080//rest/emp/create
 	// request {"id":1,"name":"Sathish
 	// Kariyanna","createdDate":"2018-06-24T20:25:58.096+0000"}
+	@ApiOperation(value = "create employee by request body")
 	@RequestMapping(value = "/rest/emp/create", method = RequestMethod.POST, produces = "application/json")
 	public @ResponseBody Employee createEmployee(@RequestBody Employee emp) {
 		System.out.println("Start createEmployee.");
@@ -70,6 +75,7 @@ public class EmployeeController {
 
 	// post data by Path Variable
 	// URL http://localhost:8080/rest/emp/createbypathvariable/2/sathish
+	@ApiOperation(value = "create employee by path variables")
 	@RequestMapping(value = "/rest/emp/createbypathvariable/{id}/{name}", method = RequestMethod.POST, produces = "application/json")
 	public @ResponseBody Employee createEmployeeByParam(@PathVariable("id") int empId,
 			@PathVariable("name") String empName) {
@@ -84,6 +90,7 @@ public class EmployeeController {
 
 	// post data by Query Parameter
 	// URL http://localhost:8080/rest/emp/createbyqueryparam?id=3&name=Kariyanna
+	@ApiOperation(value = "create employee by request parameters")
 	@RequestMapping(value = "/rest/emp/createbyqueryparam", method = RequestMethod.POST, produces = "application/json")
 	public @ResponseBody Employee createEmployeeByQueryParam(@RequestParam("id") int empId,
 			@RequestParam("name") String empName) {
@@ -99,6 +106,7 @@ public class EmployeeController {
 	}
 
 	// URL http://localhost:8080/rest/emps
+	@ApiOperation(value = "get all employees")
 	@RequestMapping(value = "/rest/emps", method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody List<Employee> getAllEmployees() {
 		System.out.println("Start getAllEmployees.");
@@ -113,6 +121,7 @@ public class EmployeeController {
 	// URL http://localhost:8080//rest/emp/update
 	// request
 	// {"id":1,"name":"Sathish","createdDate":"2018-06-25T20:25:58.096+0000"}
+	@ApiOperation(value = "update employee by using request body")
 	@RequestMapping(value = "/rest/emp/update", method = RequestMethod.PUT, produces = "application/json")
 	public @ResponseBody Employee updateEmployee(@RequestBody Employee emp) {
 		System.out.println("Start updateEmployee.");
@@ -122,6 +131,7 @@ public class EmployeeController {
 	}
 
 	// URL http://localhost:8080//rest/emp/delete/1
+	@ApiOperation(value = "delete employee by ID")
 	@RequestMapping(value = "/rest/emp/delete/{id}", method = RequestMethod.DELETE)
 	public @ResponseBody Employee deleteEmployee(@PathVariable("id") int empId) {
 		System.out.println("Start delete Employee.");
